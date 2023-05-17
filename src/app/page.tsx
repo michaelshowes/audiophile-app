cd4fcf592878487b00d2a628c65e59a24f7e41db
Michael Showes <michael.showes@gmail.com>
2023-05-17 03:14:40 -0400

    iniitial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
+import Heading from '@components/ui/Heading/Heading';
+
 type Props = {};
 export default function Home({}: Props) {
-	return <div>Home</div>;
+	return (
+		<div>
+			<Heading type='h1'>Heading 1</Heading>
+			<Heading type='h2'>Heading 2</Heading>
+			<Heading type='h3'>Heading 3</Heading>
+			<Heading type='h4'>Heading 4</Heading>
+			<Heading type='h5'>Heading 5</Heading>
+			<Heading type='h6'>Heading 6</Heading>
+		</div>
+	);
 }
