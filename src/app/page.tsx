373084a46cfc203848f80fb7ce7dc85417ac1b60
Michael Showes <michael.showes@gmail.com>
2023-05-18 02:24:20 -0400

    test page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import Heading from '@components/ui/Heading/Heading';
+import TextInput from '@components/ui/TextInput/TextInput';
 
 type Props = {};
 export default function Home({}: Props) {
 	return (
 		<div>
-			<Heading type='h1'>Heading 1</Heading>
-			<Heading type='h2'>Heading 2</Heading>
-			<Heading type='h3'>Heading 3</Heading>
-			<Heading type='h4'>Heading 4</Heading>
-			<Heading type='h5'>Heading 5</Heading>
-			<Heading type='h6'>Heading 6</Heading>
+			<TextInput
+				label='Name'
+				placeholder='Insert your name'
+			/>
 		</div>
 	);
 }
